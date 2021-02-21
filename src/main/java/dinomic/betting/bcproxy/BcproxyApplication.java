064f5bebfa25a0c41ee77eccf4743bd60d597fd1
Dinomic <hthangnguyen1101@gmail.com>
2021-02-21 21:56:05 +0700

    init spring boot project

---
+++ src/main/java/dinomic/betting/bcproxy/BcproxyApplication.java
@@ -0,0 +1,13 @@
+package dinomic.betting.bcproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcproxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcproxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dinomic/betting/bcproxy/BcproxyApplicationTests.java
@@ -0,0 +1,13 @@
+package dinomic.betting.bcproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcproxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
